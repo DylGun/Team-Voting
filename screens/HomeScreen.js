1dea3b6de166567776d28819f06bcaeea5eb8bbd
DylGun <78462130+DylGun@users.noreply.github.com>
2021-10-13 17:55:28 -0700

    Changed the background color from coral to gray

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"coral",
+    backgroundColor:"gray",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
