ef40639436069b92bf04b7e2aa0957cfd9eccda8
DylGun <78462130+DylGun@users.noreply.github.com>
2021-10-13 17:58:05 -0700

    Added Firebase Console

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyAfx1cRL9Pi4H-5k7Nwpx1uDIWoaDbz2Es",
+  authDomain: "project-67-88d07.firebaseapp.com",
+  projectId: "project-67-88d07",
+  storageBucket: "project-67-88d07.appspot.com",
+  messagingSenderId: "798724714126",
+  appId: "1:798724714126:web:28d5e8ef914dd284c423df"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
